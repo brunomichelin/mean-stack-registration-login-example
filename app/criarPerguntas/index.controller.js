5a0d65698b5696cca00997560beaab178f42aad7
brunomichelin <michelin.brunohenrique@hotmail.com>
2019-09-09 20:54:57 -0300

    subida

---
+++ app/criarPerguntas/index.controller.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CriarPerguntas.IndexController', Controller);
+
+    function Controller($window, PerguntaService, FlashService) {
+        var vm = this;
+
+        vm.pergunta = null;
+        vm.savePergunta = savePergunta;
+        vm.cancelarPergunta = cancelarPergunta;
+
+        initController();
+
+        function initController() {
+            // get current pergunta
+            PerguntaService.GetCurrent().then(function (pergunta) {
+                vm.pergunta = pergunta;
+            });
+        }
+
+        function savePergunta() {
+            PerguntaService.Create(vm.pergunta)
+                .then(function () {
+                    FlashService.Success('Pergunta salva');
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });
+        }
+
+        function cancelarPergunta() {
+            $window.location = '/home';
+        }
+    }
+
+})();
---
+++ controllers/api/perguntas.controller.js
@@ -0,0 +1,85 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var perguntaService = require('services/pergunta.service');
+
+// routes
+//router.post('/authenticate', authenticatePergunta);
+router.post('/register', registerPergunta);
+router.get('/current', getCurrentPergunta);
+router.put('/:_id', updatePergunta);
+router.delete('/:_id', deletePergunta);
+
+module.exports = router;
+
+function authenticatePergunta(req, res) {
+    perguntaService.authenticate(req.body.pergunta, req.body.pergunta)
+        .then(function (response) {
+            if (response) {
+                // authentication successful
+                res.send({ perguntaId: response.perguntaId, token: response.token });
+            } else {
+                // authentication failed
+                res.status(401).send('Perguntaname or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function registerPergunta(req, res) {
+    perguntaService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentPergunta(req, res) {
+    perguntaService.getById(req.session.perguntaId)
+        .then(function (pergunta) {
+            if (pergunta) {
+                res.send(pergunta);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updatePergunta(req, res) {
+    var perguntaId = req.session.perguntaId;
+    if (req.params._id !== perguntaId) {
+        // can only update own account
+        return res.status(401).send('You can only update your own account');
+    }
+
+    perguntaService.update(perguntaId, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deletePergunta(req, res) {
+    var perguntaId = req.session.perguntaId;
+    if (req.params._id !== perguntaId) {
+        // can only delete own account
+        return res.status(401).send('You can only delete your own account');
+    }
+
+    perguntaService.delete(perguntaId)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
